3f955a6aeb9e052facbd5fa46b0c3173cba08875
kalyan <kalyanganta84@gmail.com>
2024-08-02 18:20:55 +0530

    This is first commit

---
+++ src/example/firstpro.java
@@ -0,0 +1,13 @@
+package example;
+
+public class firstpro {public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	int i=1;
+	String s="first";
+	String a = "demo";
+	System.out.println(i);
+	System.out.println(s);
+	System.out.println(a);
+
+}
+}
